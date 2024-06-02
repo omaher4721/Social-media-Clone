c07fced2fd30e74be155d9bc06cbcafd496c3fcc
om aher <omaher2605@gmail.com>
2024-06-02 15:23:34 +0530

    add cron

--- backend/server.js
+++ backend/server.js
@@ -9,12 +9,14 @@ import postRoutes from "./routes/postRoutes.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
+import job from "./cron/cron.js";
 
 dotenv.config();
 
 // console.log("Environment Variables Loaded");
 
 connectDB();
+job.start();
 
 const port = process.env.PORT || 5000;
 
