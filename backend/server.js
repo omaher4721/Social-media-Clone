c307b3e20e13056da2bef8bed976a06dc3dd2c82
om aher <omaher2605@gmail.com>
2024-06-02 14:51:39 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import cookieParser from "cookie-parser";
 import path from "path";
 import dotenv from "dotenv";
+import { fileURLToPath } from 'url'; // Import fileURLToPath from 'url'
 import connectDB from "./db/connectDB.js";
 import userRoutes from "./routes/userRoutes.js";
 import postRoutes from "./routes/postRoutes.js";
@@ -9,22 +10,23 @@ import messageRoutes from "./routes/messageRoutes.js";
 import { v2 as cloudinary } from "cloudinary";
 import { app, server } from "./socket/socket.js";
 
-
-
 dotenv.config();
 
 // console.log("Environment Variables Loaded");
 
 connectDB();
 
-
 const port = process.env.PORT || 5000;
+
+// Define __filename and __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
-})
+});
 
 // Middlewares
 app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
@@ -36,6 +38,13 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
 
+// to run frontend and backend from single server
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+    });
+}
 
-server.listen(port, () =>  console.log(`Server is started at http://localhost:${port}`));
+server.listen(port, () => console.log(`Server is started at http://localhost:${port}`));
